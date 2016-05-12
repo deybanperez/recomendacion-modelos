070fac80aa11fca53d99c37792eb26590a2cd2b4
Deyban Perez <deyban.perez@gmail.com.com>
2016-05-12 15:54:35 -0430

    no message

--- Source/functions.R
+++ Source/functions.R
@@ -3,12 +3,52 @@ subString = function(element)
   return (substring(element, 5))
 }
 
+
 changeType = function(element)
 {
   return(as.numeric(element))
 }
 
-assignItems = function(element)
+changeFormat = function(element)
 {
-  return(element%%9)
+  classes = c("deportes", "politica", "variedades", "internacional",
+              "nacionales", "sucesos", "comunidad", "negocios", "opinion")
+  
+  article = element%%9; 
+  class = element%/%9 + 1;
+  
+  if(article == 0)
+  {
+    article = 9;
+    class = class-1;
+  }
+  
+  return(paste(classes[class], "/", "articulo",article, sep = ""))
+}
+
+convertFormat = function(element)
+{
+  items = as.integer(element)
+  newItems = changeFormat(items[1])
+  
+  if(length(items) == 1)
+  {
+    return(newItems)
+    
+  }else
+  {
+    for(i in 2:length(items))
+    {
+      newItems = paste(newItems,",", changeFormat(items[i]), sep = "")
+    }
+    
+    return(newItems)
+  }
+}
+
+generate_ROC = function(scores, real, target)
+{
+  scores = order(scores, decreasing = TRUE)
+  
+  
 }
--- Source/script.R
+++ Source/script.R
@@ -1,7 +1,6 @@
 #Changing the work space
-setwd("C:/Users/deyban.perez/Documents/Repos/Recomendacion/Source")
-source(file = "C:/Users/deyban.perez/Documents/Repos/Recomendacion/Source/functions.R")
-#Reading datasets
+setwd("C:/Users/deyban.perez/Documents/Repos/Recomendacion")
+source(file = "Source/functions.R")#Reading datasets
 df_ejemplo = read.csv("../ejemplo.csv", sep = ",")
 df_periodico = read.csv("../periodico.csv", sep = ",")
 #Here we will start with the logic to detect bots
@@ -24,11 +23,22 @@ nrow(df_periodico)
 ######################################################################
 #Now we will start with the items from homework
 #1) Modify order in the transactions
-items = lapply(split, subString)
-items = lapply(items, changeType)
-articles = lapply(items, assignItems)
-classes = 
-class = class[articles == 0] - 1
-class
-classes = c("deportes", "politica", "variedades", "internacional",
-            "nacionales", "sucesos", "comunidad", "negocios", "opinion")
+colnames(df_periodico)[5] = "items"
+split = split[-bots]
+articles = lapply(split, subString)
+df_periodico$articles = lapply(articles, convertFormat)
+#3)
+articles = df_periodico$articles
+class(articles[[1]])
+list = lapply(articles,strsplit, split = ",")
+list = lapply(list, unlist)
+transactions = as(list, "transactions")
+sort(itemFrequency(transactions, type = "absolute"), decreasing = TRUE)[1:10]
+diff = diff[-bots]
+shortest_visit = df_periodico[order(diff),c(2, 6)][1:10,2]
+highest_visit = df_periodico[order(diff, decreasing = TRUE),c(2, 6)][1:10,2]
+highest_visit
+shortest_visit
+##############################################################################
+#ALgoritmo para las curvas ROC
+##############################################################################
